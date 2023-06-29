be1f3b5df922ec35ad5499603aa3f82bde9840a0
aashumali7 <aashishmali223@gmail.com>
2023-06-29 07:36:02 +0530

    new

--- js/script.js
+++ js/script.js
@@ -16,6 +16,12 @@ let logout = ()=>{
     // page reload
     window.location.reload();
 }
+
+let playsound =()=>{
+     //console.log('good morning');
+    let at =document.querySelector('.a_audio');
+    at.play();
+}
 // ()(); IIFE
 (()=>{
     let fn = window.localStorage.getItem('first_name');
@@ -38,5 +44,66 @@ let logout = ()=>{
   if(fn !== null){
         document.querySelector('h5.a_welcome').innerHTML = 'welcome '+fn+ " "+ln+'<button class=""btn btn-sm btn-dark ms-4" onclick="logout()" >Logout</button>';
   }
- 
+       // keypress sound play 
+       document.addEventListener('keypress',(e)=>{
+        console.log('->',e.keyCode);
+      if(e.keyCode == 97){
+        let a = document.querySelector('.a_a');
+      console.log(a.classList.add('a_active'));
+       
+        document.querySelector('.l_pinky').style.display = 'block';
+      }
+      if(e.keyCode == 113){
+        let a = document.querySelector('.a_q');
+      console.log(a.classList.add('a_active'));
+       
+        document.querySelector('.l_pinky').style.display = 'block';
+      }
+      if(e.keyCode == 122){
+        let a = document.querySelector('.a_z');
+      console.log(a.classList.add('a_active'));
+       
+        document.querySelector('.l_pinky').style.display = 'block';
+      }
+
+      if(e.keyCode == 119){
+        let a = document.querySelector('.a_w');
+      console.log(a.classList.add('a_active'));
+       
+        document.querySelector('.l_ring').style.display = 'block';
+      }
+      if(e.keyCode == 115){
+        let a = document.querySelector('.a_s');
+      console.log(a.classList.add('a_active'));
+       
+        document.querySelector('.l_ring').style.display = 'block';
+      }
+      if(e.keyCode == 120){
+        let a = document.querySelector('.a_x');
+      console.log(a.classList.add('a_active'));
+       
+        document.querySelector('.l_ring').style.display = 'block';
+      }
+
+        playsound();
+       })
+       document.addEventListener('keyup',(e)=>{
+        console.log(e);
+        var elements = document.getElementsByClassName('a_active');
+         
+        for (var i = 0; i < elements.length; i++){
+            elements[i].classList.remove('a_active')
+        }
+
+        var elements = document.getElementsByClassName('a_left_pinky');
+         
+        for (var i = 0; i < elements.length; i++){
+            elements[i].style.display="none";
+        } 
+
+       // document.querySelector('.a_left_pinky').style.display = 'none';
+       })
+      
 })();
+
+// IIFE 
